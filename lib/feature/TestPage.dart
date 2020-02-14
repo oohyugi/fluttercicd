849e54e65b295732fa63216b8ad2ddc9b623317a
oohyugi <yogi.stickearn@gmail.com>
2020-02-14 16:23:55 +0700

    test

--- lib/feature/TestPage.dart
+++ lib/feature/TestPage.dart
@@ -24,7 +24,7 @@ class _TestPageState extends State<TestPage> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
-      child: Text("Test"),
+      child: Text("Testhhh"),
     );
   }
 }
