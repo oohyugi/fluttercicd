cc0fa8922cfe43fdb62c038c789c1e6b0451b11f
oohyugi <yogi.stickearn@gmail.com>
2020-02-14 15:15:06 +0700

    feature test

---
+++ lib/feature/TestPage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class TestPage extends StatefulWidget {
+  TestPage({Key key}) : super(key: key);
+
+  @override
+  _TestPageState createState() {
+    return _TestPageState();
+  }
+}
+
+class _TestPageState extends State<TestPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
